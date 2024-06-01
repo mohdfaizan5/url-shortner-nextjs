7767f3a463246497732514b919e1f9419f2c5d21
Mohammed Faizan <mohdfaizan.zprb2@gmail.com>
2024-06-02 00:55:30 +0530

    implement and work stuff

--- src/app/(main)/analytics/page.tsx
+++ src/app/(main)/analytics/page.tsx
@@ -1,8 +1,13 @@
+import Image from 'next/image'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className='p-10'>
+      <h1 className='mb-10'>Analytics</h1>
+      <Image src={"/undraw_data_report_re_p4so.svg"} height={200} width={200} alt=''/>
+
+    </div>
   )
 }
 
--- src/app/(main)/home/page.tsx
+++ src/app/(main)/home/page.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div className="flex items-center p-10 gap-10">
+      <section className="flex flex-col gap-5">
+        <Card className="flex flex-col">
+          <CardHeader>
+            <CardTitle>Make links short</CardTitle>
+            {/* <CardDescription>Card Description</CardDescription> */}
+          </CardHeader>
+          <CardFooter>
+            <Button className="w-full">Go to links</Button>
+          </CardFooter>
+        </Card>
+        <Card className="flex flex-col">
+          <CardHeader>
+            <CardTitle>Make it scannable</CardTitle>
+            {/* <CardDescription>Card Description</CardDescription> */}
+          </CardHeader>
+          <CardFooter>
+            <Button className="w-full">Go to Codes</Button>
+          </CardFooter>
+        </Card>
+      </section>
+      <section>
+        <Image
+          src={"/undraw_link_shortener_mvf6.svg"}
+          height={200}
+          width={200}
+          alt=""
+        />
+      </section>
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
